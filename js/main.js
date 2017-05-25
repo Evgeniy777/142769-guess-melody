44b8724c4e9ce104ee3954f1b34c595479ab1d0c
Evgenii Levashov <evgenii.levashov@gmail.com>
2017-05-26 01:52:04 +0300

    eslint fix

--- js/main.js
+++ js/main.js
@@ -1,38 +1,37 @@
-let templates = document.querySelector('#templates');
+let templates = document.querySelector(`#templates`);
 let templateContent = templates.content;
-let screen = templateContent.querySelectorAll('.main');
+let screen = templateContent.querySelectorAll(`.main`);
 let screens = [];
 
-for(let i = 0; i < screen.length; i++) {
-    screens[i] = screen[i];
+for (let i = 0; i < screen.length; i++) {
+  screens[i] = screen[i];
 }
 
 function showScreen(screen) {
-    let screenContainer = document.querySelector('.app .main');
-    screenContainer.innerHTML = '';
-    screenContainer.appendChild(screen);
+  let screenContainer = document.querySelector(`.app .main`);
+  screenContainer.innerHTML = ``;
+  screenContainer.appendChild(screen);
 }
 
 let startPosition = 4;
 
 showScreen(screens[startPosition]);
 
-
-document.addEventListener('keydown', (e) => {
-    if (e.altKey && e.keyCode == 37) {
-        if (startPosition > 0) {
-            startPosition--;
-        } else {
-            startPosition = screen.length - 1;
-        }
+document.addEventListener(`keydown`, (e) => {
+  if (e.altKey && e.keyCode === 37) {
+    if (startPosition > 0) {
+      startPosition--;
+    } else {
+      startPosition = screen.length - 1;
     }
-    if (e.altKey && e.keyCode == 39) {
-        if (startPosition < screen.length - 1) {
-             startPosition++;
-        } else {
-            startPosition = 0;
-        }
+  }
+  if (e.altKey && e.keyCode === 39) {
+    if (startPosition < screen.length - 1) {
+      startPosition++;
+    } else {
+      startPosition = 0;
     }
-    showScreen(screens[startPosition]);
-    return startPosition;
+  }
+  showScreen(screens[startPosition]);
+  return startPosition;
 });
