7dceef952cba1a353d9657a8bcec6dbf2dbc474f
Evgenii Levashov <evgenii.levashov@gmail.com>
2017-06-07 23:27:52 +0300

    module3-task1

--- js/game.js
+++ js/game.js
@@ -1,53 +1,71 @@
 export default {
-  screens: {
-    welcome: {
+  game: {
+    rules: {
       title: `Правила игры`,
       text: `Правила просты&nbsp;— за&nbsp;2 минуты дать максимальное количество правильных ответов.<br> Удачи!`
     },
-    levelArtist: {
-      title: `Кто исполняет эту песню?`,
-      songs: [
-        {
-          index: 1,
-          artist: `Пелагея`,
-          img: `http://lorempixel.com/400/200`
-        },
-        {
-          index: 2,
-          artist: `Краснознаменная дивизия имени моей бабушки`,
-          img: `http://lorempixel.com/300/200`
-        },
-        {
-          index: 3,
-          artist: `Lorde`,
-          img: `http://lorempixel.com/200/200`
-        },
-      ],
-    },
-    levelGenre: {
-      title: `Выберите инди-рок треки`,
-      answers: [
-        {value: 1},
-        {value: 2},
-        {value: 3},
-        {value: 4},
-      ],
-    },
-    resultWin: {
-      title: `Вы настоящий меломан!`,
-      results: {
-        time: 2,
-        melodies: 4,
-        ratio: 80
+    types: [
+      'guessArtist',
+      'guessSong'
+    ],
+    questions: [
+      {
+        type: 'guessArtist',
+        text: `Кто исполняет эту песню?`,
+        options: [
+          {
+            artist: `Пелагея`,
+            src: `http://lorempixel.com/400/200`
+          },
+          {
+            artist: `Краснознаменная дивизия имени моей бабушки`,
+            src: `http://lorempixel.com/300/200`
+          },
+          {
+            artist: `Lorde`,
+            src: `http://lorempixel.com/200/200`
+          }
+        ]
+      },
+      {
+        type: 'guessSong',
+        text: `Выберите инди-рок треки`,
+        options: [
+          {value: 1},
+          {value: 2},
+          {value: 3},
+          {value: 4}
+        ]
       }
+    ],
+    result: {
+      type: {
+        win: `Вы настоящий меломан!`,
+        fail: `Вы проиграли`
+      },
+      time: 2,
+      melodies: 4,
+      ratio: 80
+    }
+  },
+  question: [
+    'type',
+    'text',
+    'options'
+  ],
+  answer: {
+    type: {
+      correct: true,
+      wrong: false
     },
-    resultFail: {
-      title: `Вы проиграли`,
-      stat: `Ничего, вам повезет в следующий раз`
-    },
+    content: [
+      'text',
+      'src'
+    ]
   },
   timer: {
     min: `02`,
     sec: `00`
   }
 };
+
